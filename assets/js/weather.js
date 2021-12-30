de83910859b3042e7991b71453d9f832fb5c7730
lolmokha <mkhan0138@gmail.com>
2021-12-30 13:31:48 +0400

    Kanye quotes?"

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,78 +1,87 @@
-// ┬ ┬┌─┐┌─┐┌┬┐┬ ┬┌─┐┬─┐
-// │││├┤ ├─┤ │ ├─┤├┤ ├┬┘
-// └┴┘└─┘┴ ┴ ┴ ┴ ┴└─┘┴└─
+// // ┬ ┬┌─┐┌─┐┌┬┐┬ ┬┌─┐┬─┐
+// // │││├┤ ├─┤ │ ├─┤├┤ ├┬┘
+// // └┴┘└─┘┴ ┴ ┴ ┴ ┴└─┘┴└─
 
-const iconElement = document.querySelector('.weatherIcon');
-const tempElement = document.querySelector(
-  '.weatherValue p'
-);
-const descElement = document.querySelector(
-  '.weatherDescription p'
-);
+// const iconElement = document.querySelector('.weatherIcon');
+// const tempElement = document.querySelector(
+//   '.weatherValue p'
+// );
+// const descElement = document.querySelector(
+//   '.weatherDescription p'
+// );
 
-// App data
-const weather = {};
-weather.temperature = {
-  unit: 'celsius',
-};
+// // App data
+// const weather = {};
+// weather.temperature = {
+//   unit: 'celsius',
+// };
 
-var tempUnit = CONFIG.weatherUnit;
+// var tempUnit = CONFIG.weatherUnit;
 
-const KELVIN = 273.15;
-// Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = `${CONFIG.weatherKey}`;
+// const KELVIN = 273.15;
+// // Use your own key for the Weather, Get it here: https://openweathermap.org/
+// const key = `${CONFIG.weatherKey}`;
 
-// Set Position function
-setPosition();
+// // Set Position function
+// setPosition();
 
-function setPosition(position) {
-  if (!CONFIG.trackLocation || !navigator.geolocation) {
-    if (CONFIG.trackLocation) {
-      console.error('Geolocation not available');
-    }
-    getWeather(CONFIG.defaultLatitude, CONFIG.defaultLongitude);
-    return;
-  }
-  navigator.geolocation.getCurrentPosition(
-    (pos) => {
-      getWeather(
-        pos.coords.latitude.toFixed(3),
-        pos.coords.longitude.toFixed(3)
-      );
-    },
-    (err) => {
-      console.error(err);
-      getWeather(CONFIG.defaultLatitude, CONFIG.defaultLongitude);
-    }
-  );
-}
+// function setPosition(position) {
+//   if (!CONFIG.trackLocation || !navigator.geolocation) {
+//     if (CONFIG.trackLocation) {
+//       console.error('Geolocation not available');
+//     }
+//     getWeather(CONFIG.defaultLatitude, CONFIG.defaultLongitude);
+//     return;
+//   }
+//   navigator.geolocation.getCurrentPosition(
+//     (pos) => {
+//       getWeather(
+//         pos.coords.latitude.toFixed(3),
+//         pos.coords.longitude.toFixed(3)
+//       );
+//     },
+//     (err) => {
+//       console.error(err);
+//       getWeather(CONFIG.defaultLatitude, CONFIG.defaultLongitude);
+//     }
+//   );
+// }
 
 // Get the Weather data
-function getWeather(latitude, longitude) {
-  let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=${CONFIG.language}&appid=${key}`;
+function getWeather() {
+  // let api = `https://api.kanye.rest/`;
 
-  console.log(api);
+  // console.log(api);
 
-  fetch(api)
-    .then(function (response) {
-      let data = response.json();
-      return data;
-    })
-    .then(function (data) {
-      let celsius = Math.floor(data.main.temp - KELVIN);
-      weather.temperature.value =
-        tempUnit == 'C' ? celsius : (celsius * 9) / 5 + 32;
-      weather.description = data.weather[0].description;
-      weather.iconId = data.weather[0].icon;
-    })
-    .then(function () {
-      displayWeather();
-    });
+  // fetch(api)
+  //   .then(function (response) {
+  //     let data = response.json();
+  //     return data;
+  //   })
+  //   .then(function (data) {
+  //     // let celsius = Math.floor(data.main.temp - KELVIN);
+  //     // weather.temperature.value =
+  //     //   tempUnit == 'C' ? celsius : (celsius * 9) / 5 + 32;
+  //     // weather.description = data.weather[0].description;
+  //     // weather.iconId = data.weather[0].icon;
+  //     let quote = data[1]
+  //   })
+  //   .then(function () {
+  //     displayWeather();
+  //   });
+  let quotes = document.getElementById("quotes")
+  fetch (`https://api.kanye.rest/`)
+  .then (result => result.json())
+  .then (quote => {
+    quotes.innerHTML +=`<p>${quote.quote}</p>`
+  })
 }
 
-// Display Weather info
-function displayWeather() {
-  iconElement.innerHTML = `<img src="assets/icons/${CONFIG.weatherIcons}/${weather.iconId}.png"/>`;
-  tempElement.innerHTML = `${weather.temperature.value}°<span class="darkfg">${tempUnit}</span>`;
-  descElement.innerHTML = weather.description;
-}
+// // Display Weather info
+// function displayWeather() {
+//   //iconElement.innerHTML = `<img src="assets/icons/${CONFIG.weatherIcons}/${weather.iconId}.png"/>`;
+//   //tempElement.innerHTML = `${weather.temperature.value}°<span class="darkfg">${tempUnit}</span>`;
+//   //descElement.innerHTML = weather.description;
+  
+
+// }
